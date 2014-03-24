e6d63e5f2a87d04499209adb4962ab62933e22b7
Stefan Åhman <stefan.ahman@gmail.com>
2014-03-24 04:42:14 +0100

    Adds express, ejs, and stylus. Renames server.js to app.js

--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req,resp) {
-	resp.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
-	resp.write("Hallå elle! Testar lite");
-	resp.end();
-}).listen(8080);

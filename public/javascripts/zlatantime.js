dd0049101a081cd216785f990162264428cb0669
Stefan Åhman <stefan.ahman@gmail.com>
2014-03-24 12:37:38 +0100

    Adds sounds and rearranges methods

--- public/javascripts/zlatantime.js
+++ public/javascripts/zlatantime.js
@@ -1,7 +1,7 @@
 window.setTimeout("updateTime()", 0);// start immediately
 window.setInterval("updateTime()", 1000);// update every second
 function updateTime() {
-    $zlatantime('#zclock').toZTime(new Date().toTimeString().substring(0, 8));
+  $zlatantime('#zclock').refresh(new Date().toTimeString().substring(0, 8));
 }
 
 (function (window, undefined) {
@@ -35,20 +35,19 @@ function updateTime() {
     getZMinuteFromSeconds: function(seconds) {
       return parseInt((seconds - (this.getZMatchFromSeconds(seconds) * constants.match_in_seconds)) / constants.minute_in_seconds);
     },
-    toZTime: function(properties) {
-      for (var i = 0; i < this.length; i++) {
-        var res = properties.split(":");
-        var totalTimeInSeconds = parseInt(res[0]) * constants.hour_in_seconds;
-        totalTimeInSeconds += parseInt(res[1]) * constants.minute_in_seconds;
-        var second = parseInt(res[2]);
-
-        var zmatch = this.getZMatchFromSeconds(totalTimeInSeconds);
-        var zminute = this.getZMinuteFromSecondsWithMatch(zmatch, totalTimeInSeconds);
-
-        var time = this.toString(zmatch, zminute, second);
-        $(this[i]).text(time);
+    toZTime: function(time) {
+      var res = time.split(":");
+      var totalTimeInSeconds = parseInt(res[0]) * constants.hour_in_seconds;
+      totalTimeInSeconds += parseInt(res[1]) * constants.minute_in_seconds;
+      var second = parseInt(res[2]);
+      var zmatch = this.getZMatchFromSeconds(totalTimeInSeconds);
+      var zminute = this.getZMinuteFromSecondsWithMatch(zmatch, totalTimeInSeconds);
+      if (zminute === 0 && second === 0) {
+        this.playSound('sounds/whistle_start');
+      } else if (zminute === 45 && second === 0) {
+        this.playSound('sounds/whistle_half');
       }
-      return this;
+      return this.toString(zmatch, zminute, second);
     },
     toString: function(matchOrHour, minute, second) {
       var time = ""
@@ -68,6 +67,13 @@ function updateTime() {
         time += ':00';
       }
       return time;
+    },
+    playSound: function() {
+      return $("<embed src='"+arguments[0]+".mp3' hidden='true' autostart='true' loop='false' class='playSound'>" + "<audio autoplay='autoplay' style='display:none;' controls='controls'><source src='"+arguments[0]+".mp3' /></audio>").appendTo('body');
+    },
+    refresh: function(time) {
+      for (var i = 0; i < this.length; i++)
+        $(this[i]).text(this.toZTime(time));
     }
   };
   window.$zlatantime = $zlatantime;
